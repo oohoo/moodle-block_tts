ac1c2153ebcd934312a7d76297c5eda751e22853
Nicolas Bretin <bretin@ualberta.ca>
2012-10-30 09:32:47 -0600

    Add a TTS test page to test the apache server connectivity. Add info in the Readme

---
+++ tts_test.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * *************************************************************************
+ * *                    OOHOO - TTS - Text To Speech                      **
+ * *************************************************************************
+ * @package     block                                                     **
+ * @subpackage  TTS                                                       **
+ * @name        TTS                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Ryan Thomas (Original Author)                             **
+ * @author      Dustin Durand                                             **
+ * @author      Nicolas Bretin                                            **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+require_once('../../config.php');
+
+global $CFG, $OUTPUT, $PAGE;
+
+$context = get_context_instance(CONTEXT_SYSTEM, 0);
+
+require_login(1);
+
+$PAGE->set_title('TTS Test Page');
+$PAGE->set_url('/blocks/tts/tts_test.php');
+$PAGE->navbar->add('TTS Test Page');
+
+echo $OUTPUT->header();
+
+
+$testgoogle = @get_headers("http://translate.google.com/translate_tts");
+$testmicrosoft = @get_headers("http://api.microsofttranslator.com/V2/http.svc/Speak");
+
+if($testgoogle === false)
+{
+    echo 'Fail on retreiving information from Google server. Please check your server connectivity. (Apache server must have a gateway).<br/>';
+}
+else
+{
+    echo 'There is no problem to access to the Google server.<br/>';
+}
+if($testmicrosoft === false)
+{
+    echo 'Fail on retreiving information from Microsoft server. Please check your server connectivity. (Apache server must have a gateway).<br/>';
+}
+else
+{
+    echo 'There is no problem to access to the Microsoft server.<br/>';
+}
+
+echo $OUTPUT->footer();
+?>
